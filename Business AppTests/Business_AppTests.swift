3d8152f1c1c043c36b159b740a18a19f6da9e92b
apramodya <pramodyaabeysinghe@gmail.com>
2019-03-21 01:13:09 +0800

    Initial Commit

---
+++ Business AppTests/Business_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Business_AppTests.swift
+//  Business AppTests
+//
+//  Created by Pramodya Abeysinghe on 21/3/19.
+//  Copyright © 2019 Pramodya. All rights reserved.
+//
+
+import XCTest
+@testable import Business_App
+
+class Business_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

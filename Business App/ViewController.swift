403d0321d1d5a667adc10dd54741b3cba6d07571
apramodya <pramodyaabeysinghe@gmail.com>
2019-03-21 02:29:04 +0800

    setup home view with tab navigation

--- Business App/ViewController.swift
+++ Business App/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button1.layer.cornerRadius = 5
+        button2.layer.cornerRadius = 5
+        button3.layer.cornerRadius = 5
+        button4.layer.cornerRadius = 5
+        button5.layer.cornerRadius = 5
     }
 
-
+    @IBAction func services(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 2
+    }
+    @IBAction func portfolio(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 3
+    }
+    @IBAction func aboutUs(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 1
+    }
+    @IBAction func contactUs(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 4
+    }
+    
 }
 
